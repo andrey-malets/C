738f312f3b10394df308392d014269144c8767ce
Andrey Malets <andrey.malets@gmail.com>
2014-10-25 14:05:19 +0600

    Added an ability to specify make options at Makefile generation time

--- gen-makefile.sh
+++ gen-makefile.sh
@@ -4,7 +4,8 @@
   for file in *.c; do
     echo -e "${file%%.c}: $file"
     echo -e "\tmkdir -p bin"
-    echo -e "\tclang -Wall -pedantic -o bin/${file%%.c} $file"
+    opts="$(grep '^// clang_opts: ' "$file" | cut -f2 -d:)"
+    echo -e "\tclang $opts -Wall -pedantic -o bin/${file%%.c} $file"
     echo -e "\t./bin/${file%%.c}"
   done
 } > Makefile
