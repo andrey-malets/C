d9bebaf5017ce96d5c12ff2c00b1908f1e39d1f1
Андрей Малец <uzer@ts.runc.urgu.org>
2014-11-15 14:38:36 +0500

    Add prefix and run_opts parsing to gen-makefile.sh

--- gen-makefile.sh
+++ gen-makefile.sh
@@ -4,8 +4,14 @@
   for file in *.c; do
     echo -e "${file%%.c}: $file"
     echo -e "\tmkdir -p bin"
-    opts="$(grep '^// clang_opts: ' "$file" | cut -f3- -d' ')"
-    echo -e "\tclang $opts -Wall -pedantic -o bin/${file%%.c} $file"
-    echo -e "\t./bin/${file%%.c}"
+    clang_opts="$(grep '^// clang_opts: ' "$file" | cut -f3- -d' ')"
+    prefix="$(grep '^// run_prefix: ' "$file" | cut -f3- -d' ')"
+    run_opts="$(grep '^// run_opts: ' "$file" | cut -f2- -d:)"
+    echo -e "\tclang $clang_opts -Wall -pedantic -o bin/${file%%.c} $file"
+    if ! [[ -z "$prefix" ]]; then
+        echo -e "\tbash -c \"${prefix} ./bin/${file%%.c}${run_opts}\""
+    else
+        echo -e "\t./bin/${file%%.c}${run_opts}"
+    fi
   done
 } > Makefile
