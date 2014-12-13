b6f14af19966b1c369c6e2a6c97d76ff11db02af
Андрей Малец <uzer@ts.runc.urgu.org>
2014-12-13 17:53:24 +0500

    Remove python prefix

--- 13.12/dfyz/e/perf.sh
+++ 13.12/dfyz/e/perf.sh
@@ -1,3 +1,3 @@
 #!/usrb/bin/env bash
 
-python ocperf.py stat -e mem_load_uops_llc_miss_retired.local_dram,mem_load_uops_llc_miss_retired.remote_dram ./a.out
+ocperf.py stat -e mem_load_uops_llc_miss_retired.local_dram,mem_load_uops_llc_miss_retired.remote_dram ./a.out
